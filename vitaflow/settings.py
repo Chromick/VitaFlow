07c41302f2e3fbd90ca98b910089ec60b03eef06
Chromick <brunobarral15@gmail.com>
2026-06-10 22:48:28 -0300

    Corrigindo ALLOWED_HOSTS e arquivos de teste

--- vitaflow/settings.py
+++ vitaflow/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-only-vitaflow-clinic-key")
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
